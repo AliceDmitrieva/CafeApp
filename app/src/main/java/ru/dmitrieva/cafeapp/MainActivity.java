af42f20eead7c9351dc1f529c6e2f3d194b12bdd
Alice Dmitrieva <coolalisa@yandex.ru>
2018-09-27 11:05:00 -0700

    Add bottom navigation menu

--- app/src/main/java/ru/dmitrieva/cafeapp/MainActivity.java
+++ app/src/main/java/ru/dmitrieva/cafeapp/MainActivity.java
@@ -1,10 +1,13 @@
 package ru.dmitrieva.cafeapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 
     public void addPages(ViewPager pager) {
