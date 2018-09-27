277977288970b1522ff18672dcecf63428d3edfb
Alice Dmitrieva <coolalisa@yandex.ru>
2018-09-27 12:33:53 -0700

    Add free space between cards
    Implementing by means of  ItemDecoration

--- app/src/main/java/ru/dmitrieva/cafeapp/CategoryFragment.java
+++ app/src/main/java/ru/dmitrieva/cafeapp/CategoryFragment.java
@@ -1,10 +1,14 @@
 package ru.dmitrieva.cafeapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,12 +27,22 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         View view = inflater.inflate(R.layout.content_fragment, null);
-
         RecyclerView recyclerView = view.findViewById(R.id.contentFragment);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new SpacingItemDecoration(5, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         recyclerView.setAdapter(new RecyclerAdapter(this.getActivity(), getProducts()));
 
         return view;
+    }
+
+    private int dpToPx(int dp) {
+        Resources resources = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics()));
     }
 
     private ArrayList<Product> getProducts() {
